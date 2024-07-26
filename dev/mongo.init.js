1ae4f12d2c069a2acc9634673e42f08a1edab140
w41x <_@w41x.io>
2024-07-26 19:03:08 +0200

    updated to PayloadCMS v3.0.0-beta.70

--- dev/mongo.init.js
+++ dev/mongo.init.js
@@ -11,9 +11,9 @@ try {
 }
 
 // init cms database & create user for the API if absent
-var be_cms = db.getSiblingDB('cms')
-if (!be_cms.getUser('api')) {
-    be_cms.runCommand({
+var cms = db.getSiblingDB('cms')
+if (!cms.getUser('api')) {
+    cms.runCommand({
         createUser: 'api',
         pwd: 'secret',
         roles: [{role: 'readWrite', db: 'cms'}]
